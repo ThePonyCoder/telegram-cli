7af0863e6cc431348dadc5982e02417dc4509905
ponycoder <theponycoder@gmail.com>
2020-06-14 13:19:37 +0300

    now we can draw chats

--- teletools/core/chats.py
+++ teletools/core/chats.py
@@ -8,21 +8,16 @@ class Chats:
         self.chat_list = None
         self.height, self.width = self.window.getmaxyx()
 
-
     def set_chat_list(self, chat_list):
         self.chat_list = chat_list
         if self.active_chat is None:
             self.active_chat = self.chat_list[0]
 
+        self._draw_chats(0, self.height)
+
+    def _draw_chats(self, start=0, end=1):
         self.window.erase()
-        for line, chat in enumerate(self.chat_list):
+        for line, chat in enumerate(self.chat_list[start:end]):
             self.window.insstr(line, 0, chat.name)
 
         self.window.refresh()
-        sleep(2)
-        self.window.scroll(2)
-        self.window.refresh()
-        sleep(2)
-
-
-
